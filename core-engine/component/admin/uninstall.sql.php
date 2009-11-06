81ff61afb267ae68d45c795003c8919cde585c5c
arunoda.susiripala@gmail.com <arunoda.susiripala@gmail.com@ecc0cb80-cae7-11de-a70d-3b9dd4243769>
2009-11-06 21:35:54 +0000

    Directory Structure Changed!

---
+++ core-engine/component/admin/uninstall.sql.php
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `#__jc_exApps`;
+DROP TABLE IF EXISTS `#__jc_meta`;
+DROP TABLE IF EXISTS `#__jc_syncUsers`;
+DROP TABLE IF EXISTS `#__jc_externalUsers`;
+DROP TABLE IF EXISTS `#__jc_assoc_table`;
+DROP TABLE IF EXISTS `#__jc_nonce`;
+DROP TABLE IF EXISTS `#__jc_groups_out`;
+DROP TABLE IF EXISTS `#__jc_groups_in`; 
