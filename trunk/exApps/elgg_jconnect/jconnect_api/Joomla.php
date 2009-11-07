be5c7e1272c08ce1894c39cdaf3ed1cd013601c0
arunoda.susiripala@gmail.com <arunoda.susiripala@gmail.com@ecc0cb80-cae7-11de-a70d-3b9dd4243769>
2009-11-07 10:02:51 +0000

    Core-Engine's endpoint changed for using REST/JSON XMLRPC Removed from Core-Engine's endpoint

--- trunk/exApps/elgg_jconnect/jconnect_api/Joomla.php
+++ trunk/exApps/elgg_jconnect/jconnect_api/Joomla.php
@@ -12,30 +12,17 @@ include_once('xmlrpc/xmlrpcs.inc.php');
 include_once('aes-lib.php');
 
 class Joomla{
-	public $appName;
-	public $secretKey;
-	public $host='localhost';
-	public $path='/gsoc/JConnect/exApps/fake/server.php';
-	public $port=80;
-	public $authKey;
-	public $cryptKey;
+	private $appName;
+	private $secretKey;
+	private $authKey;
+	private $cryptKey;
+	private $endpoint;
 
-	//the xmlrpc client...
-	private $client;
-	private $cookies;
 
-	public function __construct($url,$appName,$secKey){
-		$this->host=preg_replace(array('/http(s)*:\/\//','/:[a-zA-Z0-9\/\.]*/'),'',$url);
-		$this->port=preg_replace(array('/http(s)*:\/\//',"/$this->host:/","/\/[a-zA-Z0-9\/\.]*/"),'',$url);
-		$this->path="/".preg_replace(array("/http(s)*:\/\//","/$this->host/","/:{$this->port}[\/]*/"),'',$url).
-			"/?option=com_jconnect&format=raw";
-		
+	public function __construct($url,$appName,$secKey){	
 		$this->appName=$appName;
 		$this->secretKey=$secKey;
-
-		$this->client=new xmlrpc_client($this->path,$this->host,$this->port);
-		$this->client->return_type="phpvals";
-		$this->client->setDebug(0);
+		$this->endpoint=$url;
 		
 		$keys=explode("::",$secKey);
 		$this->authKey=$keys[0];
@@ -45,52 +32,44 @@ class Joomla{
 
 	public function createUser($username,$email,$password,$group='user'){
 		$password=AESEncryptCtr($password,$this->cryptKey,256);
-		return $this->callMethod("jc.host.createUser",array($username,$email,$password,$group));
+		return $this->callMethod("createUser",array(
+			'username'=>$username,
+			'email'=>$email,
+			'password'=>$password,
+			'group'=>$group));
 	} 
 
 	public function updateUser($username,$email,$password,$group='admin'){
-		//encrypting password..
 		$password=AESEncryptCtr($password,$this->cryptKey,256);
-		return $this->callMethod("jc.host.updateUser",array($username,$email,$password,$group));
+		return $this->callMethod("updateUser",array(
+			'username'=>$username,
+			'email'=>$email,
+			'password'=>$password,
+			'group'=>$group));
 	}
 
 	public function deleteUser($username){
 
-		return $this->callMethod("jc.host.deleteUser",array($username));  
+		return $this->callMethod("deleteUser",array(
+			'username'=>$username));  
 	}
 	
 	
-	private function callMethod($methodName,$paramArray){
+	private function callMethod($action,$paramArray){
 		//generating hash value and send-it...
 		$hmac_hash=$this->hmac_gen($paramArray);
-		$msg=new xmlrpcmsg($methodName);
-		$msg->addParam(php_xmlrpc_encode($this->appName));
-		$msg->addParam(php_xmlrpc_encode($hmac_hash));
-		for($lc=0;$lc<sizeof($paramArray);$lc++){
-			$msg->addParam(php_xmlrpc_encode($paramArray[$lc]));
-		}
-
-		$res =& $this->client->send($msg, 0, '');
-
-		if($res->faultCode){
-			throw new Exception("xmlrpc Error :: ".$res->faultString, $res->faultCode);
-		}
-		else if($res->errno){
-			throw new Exception("user Error :: ".$res->errstr,$res->errno);
-		}
-
-		$varU=$res->value();
 		
-		if(!is_string($varU) && isset($varU['code'])){
-			throw new Exception("Joomla Error :: ".$varU['message'],(int)$varU['code']);
+		$paramArray['hmacHash']=$hmac_hash;
+		$paramArray['appName']=$this->appName;
+		$call=$this->endpoint."?option=com_jconnect&format=raw&action=$action&json=".json_encode($paramArray);
+		$res=file($call);
+		$res=json_decode(implode("\n",$res),true);
+		if($res->result==0){
+			return $res->data;
 		}
-		
-		//validating hash for return value..
-		$hmac_hash=$this->hmac_gen(array($varU[1]));
-		if($varU[0]!=$hmac_hash) throw new Exception ("ExApp Error: Authentication or Integrity failed!");
-		$varU=$varU[1];
-
-		return $varU;
+		else{
+			throw new Exception($res->message,$res->no);
+		}
 
 	}
 	
