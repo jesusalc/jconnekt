995c4ff3c8e9cf25baf85c7eb2353b8f06b1d321
arunoda.susiripala@gmail.com <arunoda.susiripala@gmail.com@ecc0cb80-cae7-11de-a70d-3b9dd4243769>
2010-01-23 14:18:08 +0000

    Copying Old 1.0.2 Branch into trunk

---
+++ trunk/exApps/elgg_jconnect/views/default/config/login.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* @author		Arunoda Susiripala
+* @package		jconnect
+* @subpackage	elgg
+* @copyright	Arunoda Susiripala
+* @license 		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+*/
+
+?>
+
+<div id='jconnekt_login_box'></div>
+<script type="text/javascript">
+	jconnekt.draw_login('jconnekt_login_box');
+</script>
+
+
+
+
