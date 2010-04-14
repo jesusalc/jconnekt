f9789d8a225a7d9b168821ba687816f48a4f90e8
arunoda.susiripala@gmail.com <arunoda.susiripala@gmail.com@ecc0cb80-cae7-11de-a70d-3b9dd4243769>
2010-04-14 20:38:41 +0000

    Basic Webservice-tester done. Framework is OK. Now needs testing of that.

--- projects/webservice-tester/WSTester.php
+++ projects/webservice-tester/WSTester.php
@@ -1,12 +1,22 @@
 <?php
 
 class WSTester{
-	public function __construct(){
-		
+	private $dataStore;
+	private $id=10;
+	public function __construct($dataStore){
+		$this->dataStore=$dataStore;
 	}
 	
 	public function assertTrue($value,$message){
+		$params=array
+		(
+			'method'=>'store_send',
+			'id'=>$this->id,
+			'value'=>$value,
+			'message'=>$message
+		);
 		
+		$this->sendRequest($this->dataStore,json_encode($params));
 	}
 	
 	public function assertLocalTrue($value,$message){
@@ -14,6 +24,89 @@ class WSTester{
 	}
 	
 	public function dump($value){
+		$params=array
+		(
+			'method'=>'store_recieve',
+			'value'=>$value,
+		);
 		
+		$this->sendRequest($this->dataStore,json_encode($params));
 	}
+	
+	private function sendRequest($endpoint,$json){
+		if(function_exists('curl_init')){
+		        $ch = curl_init($endpoint.'?action='.urlencode($json));
+				curl_setopt($ch, CURLOPT_HEADER, 0);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				$res = stripcslashes(curl_exec($ch));       
+				curl_close($ch);
+		}
+		else{
+		        $res=file($endpoint.'?action='.urlencode($json));
+		        $res=stripslashes(implode("\n",$res));
+		        
+		}
+		
+		return $res;
+    }
+    
+    
+    
+    public function execute(){
+    	$methods=get_class_methods($this);
+    	foreach ($methods as $method){
+    		if(substr($method,0,4)=='test'){
+    			//setting up the id
+    			$this->id=$method;
+    			$this->$method();
+    			$this->displayExecute($method);
+    		}
+    	}
+    	
+    	$response=$this->sendRequest($this->dataStore,json_encode(array('method'=>'store_pop')));
+    	
+    	$tests=json_decode(urldecode($response),true);
+    	
+    	$this->displayResultTopic();
+    	
+    	foreach ($tests as $testItem){
+    		if($testItem['value_send']==$testItem['value_received']){
+    			$this->displaySuccess($testItem);
+    		}
+    		else{
+    			$this->displayFailure($testItem);
+    		}
+    	}
+    }
+    
+    private function displayExecute($method){
+    	$str= 'executing '.substr($method,4) .'...<br>';
+    	echo '<div style="background-color:orange;color:white;padding:10px">'.
+    	$str.
+    	'</div>';
+    }
+    
+	private function displaySuccess($testItem){
+    	$str=substr($testItem['id'],4)." passed!";
+    	echo '<div style="background-color:green;color:white;padding:10px">'.
+    	$str.
+    	'</div>';
+    }
+    
+	private function displayFailure($testItem){
+    	$str=substr($testItem['id'],4).' failed!'.
+    		' [  '.$testItem['message']. '  ]<br>'. 
+    		'sent:: '.$testItem['value_send'].'<br>'.
+    		'recieved:: '.$testItem['value_received'];
+    	echo '<div style="background-color:red;color:white;padding:10px">'.
+    	$str.
+    	'</div>';	
+    }
+    
+    private function displayResultTopic(){
+    	echo "<br/><h2>Displaying Results</h2>";
+    }
+	
 }
+
+
--- projects/webservice-tester/datastore.php
+++ projects/webservice-tester/datastore.php
@@ -26,6 +26,86 @@
  * 		value_send:'the value'
  * 		value_recieve:'the_value'
  * 		message:'the_message'
+ *  },
  * ]
  * 
  */
+
+
+function store_send($id,$value,$message){
+	$file=fopen('datastore.txt','a');
+	$data=array($value,$message);
+	$storeStr=$id . '::'. $value . '::'.$message;
+	fwrite($file,$storeStr);
+	fclose($file);
+}
+
+function store_recieve($value){
+	$file=fopen('datastore.txt','a');
+	fwrite($file,'::'.$value."\n");
+	fclose($file);
+}
+
+function store_pop(){
+	$content=file('datastore.txt'); 
+	$rtn=array();
+	foreach ($content as $line){
+		$pragments=explode("::",$line);
+		$rtn[]=array
+		(
+			'id'=>$pragments[0],
+			'value_send'=>$pragments[1],
+			'value_received'=>substr($pragments[3],0,-1), //inorder to remove last endline
+			'message'=>$pragments[2]
+		);
+	}
+	
+	//deleting the store
+	$file=fopen('datastore.txt','w');
+	fwrite($file,'');
+	fclose($file);
+	
+	echo urlencode(json_encode($rtn));
+}
+
+$action=$_GET['action'];
+//var_dump($action);
+$action=json_decode(stripslashes($action),true);
+
+//var_dump($action);
+
+if($action['method']=='store_send'){
+//	echo "send";
+	store_send($action['id'],$action['value'],$action['message']);
+}
+else if($action['method']=='store_recieve'){
+//	echo "recieve";
+	store_recieve($action['value']);
+}
+else if($action['method']=='store_pop'){
+//	echo "pop<hr/>";
+	store_pop($action['value']);
+}
+
+//echo '<hr/>';
+
+/*
+$data=array(
+		'method'=>'store_send',
+		'id'=>'21',
+		'value'=>'the value',
+		'message'=>'the test message'
+	);
+	echo '<a href=\'?action='.json_encode($data).'\'>Send</a><br>';
+	
+	$data=array(
+		'method'=>'store_recieve',
+		'value'=>'the value2 recieve',
+	);
+	echo '<a href=\'?action='.json_encode($data).'\'>Receive</a><br>';
+	
+	$data=array(
+		'method'=>'store_pop',
+	);
+	echo '<a href=\'?action='.json_encode($data).'\'>Pop</a><br>';
+*/
