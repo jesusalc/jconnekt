edf915565e90ce403f8990665aaeb847d46e2338
arunoda.susiripala@gmail.com <arunoda.susiripala@gmail.com@ecc0cb80-cae7-11de-a70d-3b9dd4243769>
2010-04-17 13:59:14 +0000

    [Transport] -implement StandardInvoker without filters -create PHPUnit tests

--- trunk/projects/transport/StandardInvoker.php
+++ trunk/projects/transport/StandardInvoker.php
@@ -20,12 +20,13 @@ class StandardInvoker extends Invoker{
 
 	var $def; //as an Array
 	var $endpoint;
+	var $responses;
 	
 	/**
 	 * @param $endpoint the url where webservice stored
 	 */
 	public function __construct($endpoint){
-		
+		$this->endpoint=$endpoint;
 	}
 	
 	public function registerDef($def){
@@ -34,30 +35,53 @@ class StandardInvoker extends Invoker{
 	
 	public function invoke($method,$params){
 		//check method against the definition
+		$validateResponse=$this->validateMethod($method,$params);
+		
+		if(is_int($validateResponse)) throw new Exception("Method Signature Error",$errorCode);
+		
 		//create json-rpc syntax
+		$id=null;
+		//no id for method which there is no response
+		if($validateResponse['response']){
+			$id='StandardInvoker::'.rand(0,100);
+		}
+		$json=$this->getJsonRpc($method,$params,$id);
+		
 		//call the method
+		$response=$this->sendRequest($this->endpoint,$json);
 		//get the response store it on the array
+		$this->handleResponse($response);
 		//return the latest index of the array
+		return $id;
 	}
 	
 	public function getResponse($id){
 		//check whether id is a valid array index
 		//if the response is a error throw the exception
+		if(!isset($this->responses[$id])){
+			throw new Exception("invalid method id");
+		}
+		
+		//@todo do we want to remove the response from the array
 		//return the response
+		return $this->responses[$id];
 	}
 	
 	/**
 		This will do the actual communication
-		@param $endpoint request endpoint url (with ? )
+		@param $endpoint request endpoint url
 		@param $json json-rpc request
 		
 	 */
 	private function sendRequest($endpoint,$json){
+		//ready the endpoint to added to the parameters
+		if(strpos($endpoint,'?')===false) $endpoint.="?";
+		else $endpoint.="&";
+		
 		$res;
 		if(function_exists('curl_init')){
 			$ch = curl_init("$endpoint");
-			$params=urlencode("action").'='.urlencode($action)."&";
-			$params.=urlencode("json").'='.urlencode($json);
+			$params=urlencode("json").'='.urlencode($json);
 			curl_setopt($ch, CURLOPT_POSTFIELDS,  $params);
 	        curl_setopt($ch, CURLOPT_HEADER, 0);
 	        curl_setopt($ch, CURLOPT_POST, 1);
@@ -67,7 +91,7 @@ class StandardInvoker extends Invoker{
 	        
 		}
 		else{
-			$res=file("{$endpoint}&action=$action&json=$json");
+			$res=file("{$endpoint}json=$json");
 			$res=stripslashes(implode("\n",$res));
 			
 		}
@@ -75,27 +99,61 @@ class StandardInvoker extends Invoker{
 		return $res;
 	}
 	
-	private function validateMethod($request){
+	/**
+		validate method requested with the definition we're having
+		@return a collection of bitwise error codes (as a sum) if there is a error
+			unless the signature object
+	 */
+	private function validateMethod($method,$params){
+		if(!isset($this->def)) throw new Exception('Method Definition not supplied');
 		$match;
 		foreach ($this->def as $signature){
-			if($request['method']==$signature['method']){
-				$match=$signature;	
+			if($method==$signature['method']){
+				$match=$signature;
 				break;
 			}
 		}
 		
 		$errorCode=0;
 		
-		if(!isset($match)) $errorCode+=Registrar::$METHOD_NOT_FOUND;
+		if(!isset($match)) $errorCode+=StandardInvoker::$METHOD_NOT_FOUND;
 		
-		if(count($match['params'])!=count($request['params'])) {
-			$errorCode+=Registrar::$PARAM_COUNT_NOT_MATCHED;
+		if(count($match['params'])!=count($params)) {
+			$errorCode+=StandardInvoker::$PARAM_COUNT_NOT_MATCHED;
+		}
+	
+		if($errorCode==0){
+			return $match;
+		}else{
+			return $errorCode;
 		}
 		
-		if($match['response'] && !isset($request['id'])) {
-			$errorCode+=Registrar::$NO_VALUE_FOR_ID;
-		}
+	}
+	
+	/**
+		generates the json-rpc signature
+		@return json-rpc signature in json 
+	 */
+	private function getJsonRpc($method,$params,$id){
+		$rpc=array
+		(
+			'method'=>$method,
+			'params'=>$params,
+			'id'=>$id
+		);
 		
-		return $errorCode;
+		return json_encode($rpc);
+	}
+	
+	/*
+	 * This will store the response in the array index as the id
+	 * and throws exception any error occured
+	 */
+	private function handleResponse($response){
+		
+		$rpc=json_decode(stripslashes($response),true);
+		if(isset($rpc['error'])) throw new Exception($rpc['error']);
+		
+		$this->responses[$rpc['id']]=$rpc['result'];
 	}
 }
--- trunk/projects/transport/tests/StandardInvokerTest.php
+++ trunk/projects/transport/tests/StandardInvokerTest.php
@@ -1,32 +1,123 @@
 <?php
 require_once 'PHPUnit/Framework.php';
 include_once '../StandardInvoker.php';
- 
+
 class StandardInvokerTest extends PHPUnit_Framework_TestCase{
 	var $invoker;
 	public function setUP(){
-		$this->invoker=new StandardInvoker(null);
-	}
-	
-	public function testRegisterDef(){
+		$this->invoker=new StandardInvoker
+		('http://localhost/jconnekt/trunk/projects/transport/tests/server.php');
 		$json='['.
 			' {'.
 			' 	"method":"UserSync::createUser",'.
 			' 	"params":["username","password","email"],'.
 			' 	"filters":["encrypt","authenticate"],'.
-			' 	"response":"true"'.
+			' 	"response":true'.
 			' },'.
 			' {'.
 			' 	"method":"MuBlog::addStatus",'.
 			' 	"params":["username","status"],'.
 			' 	"filters":["authenticate"],'.
-			' 	"response":"false"'.
+			' 	"response":false'.
 			' }'.
 			']';
-		
+
 		$this->invoker->registerDef($json);
+	}
+
+	public function testRegisterDef(){
+
 		$this->assertNotNull($this->invoker->def);
 		$this->assertSame($this->invoker->def[0]['method'],"UserSync::createUser");
 		$this->assertSame(count($this->invoker->def),2);
+	}
+
+	public function testBasicInvoke(){
+		$id=$this->invoker->invoke("UserSync::createUser",array("u","p","e"));
+		$this->assertNotNull($id);
+		$response=$this->invoker->getResponse($id);
+		$this->assertNotNull($response,"UserSync::createUser");
+	}
+
+	public function testMethodSignature(){
+		try{
+			$this->invoker->invoke("UserSyn::createUser",array("u","p","e"));
+			$this->invoker->invoke("UserSync::createUser",array("p","e"));
+			$this->invoker->invoke("Userc::createUser",array("p","e"));
+			$this->invoker->invoke(null,null);
+		}
+		catch(Exception $ex){
+			return;
+		}
+		$this->fail("Method Signature Invalid exception not throwed");
+	}
+	
+	public function testDefinitionInvalidException(){
+		try{
+			$i1=new StandardInvoker
+				('http://localhost/jconnekt/trunk/projects/transport/tests/server.php');
+			$i1->invoke("UserSync::createUser",array("u","p","e"));
+		}
+		catch(Exception $ex){
+			return;
+		}
+		
+		$this->fail("not throws Exception When definition is not provided ");
+	}
+	
+
+	/**
+		@dataProvider getinvokers
+	 */
+	public function testEndpointCheck($invoker){
+
+		$id=$invoker->invoke("UserSync::createUser",array("u","p","e"));
+		$this->assertNotNull($id);
+		$res=$invoker->getResponse($id);
+		$this->assertNotNull($res);
+
+	}
+	
+
+	public function getinvokers(){
+		$i1=new StandardInvoker
+		('http://localhost/jconnekt/trunk/projects/transport/tests/server.php');
+		$i1->registerDef($this->getDef());
+		
+		$i2=new StandardInvoker
+		('http://localhost/jconnekt/trunk/projects/transport/tests/server.php?abc=100');
+		$i2->registerDef($this->getDef());
+		
+		$i3=new StandardInvoker
+		('http://localhost/jconnekt/trunk/projects/transport/tests/server.php?a=10&d=10');
+		$i3->registerDef($this->getDef());
+		return array(
+			array($i1),
+			array($i2),
+			array($i3)
+		);
+	}
+	
+	public function testGettingIdForNotifications(){
+		$id=$this->invoker->invoke("MuBlog::addStatus",array("a","a"));
+		$this->assertNull($id);
+	}
+
+	private function getDef(){
+		$json='['.
+			' {'.
+			' 	"method":"UserSync::createUser",'.
+			' 	"params":["username","password","email"],'.
+			' 	"filters":["encrypt","authenticate"],'.
+			' 	"response":true'.
+			' },'.
+			' {'.
+			' 	"method":"MuBlog::addStatus",'.
+			' 	"params":["username","status"],'.
+			' 	"filters":["authenticate"],'.
+			' 	"response":false'.
+			' }'.
+			']';
+		return $json;
 	}
 }
